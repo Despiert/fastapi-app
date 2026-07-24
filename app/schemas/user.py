7ee1a5445a99ef8b98c4429743438e317cf586e4
Despierto <nrilock@gmail.com>
2026-07-24 17:02:25 +0300

    fix validators

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,5 +1,3 @@
-from typing import Annotated
-from annotated_types import MinLen, MaxLen
 from pydantic import BaseModel, Field, EmailStr
 from datetime import datetime
 
@@ -7,7 +5,7 @@ from datetime import datetime
 class UserAdd(BaseModel):
     username: str = Field(..., min_length=3, max_length=30)
     email: EmailStr
-    password: Annotated[str, MinLen(8), MaxLen(30)]
+    password: str = Field(..., min_length=8, max_length=30)
 
 class UserGet(BaseModel):
     username: str
