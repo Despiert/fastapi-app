592b307da4dbe3c2643f9ed34ef96748105ee933
Despierto <nrilock@gmail.com>
2026-07-27 04:02:20 +0300

    feat: add password hashing with sha256_crypt

--- app/utils/hashing.py
+++ app/utils/hashing.py
@@ -1,5 +1,14 @@
-import bcrypt
+import hashlib
 
+from passlib.context import CryptContext
+
+
+pwd_context = CryptContext(schemes=['sha256_crypt'], sha256_crypt__rounds=10_000,deprecated='auto')
 
 def hash_password(password: str) -> str:
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+    password_hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
+    return pwd_context.hash(password_hash)
+
+def verify_password(password: str, hashed: str) -> bool:
+    password_hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
+    return pwd_context.verify(password_hash, hashed)
