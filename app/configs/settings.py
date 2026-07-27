bd9b2d74da5fef7b9254690d08d09fe92926e647
Despierto <nrilock@gmail.com>
2026-07-27 18:33:25 +0300

    fix:settings

--- app/configs/settings.py
+++ app/configs/settings.py
@@ -1,11 +1,12 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     DATABASE_URL: str | None = None
 
-    class Config:
-        env_file = '.env'
-        env_file_encoding = 'utf-8'
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        env_file_encoding='utf-8'
+    )
 
 
 settings = Settings()
