5b04afc194a8ee1f8646b7f8ca69c0882dfe7651
Despierto <nrilock@gmail.com>
2026-07-27 18:33:49 +0300

    cleanup: remove dead code

--- app/utils/hashing.py
+++ app/utils/hashing.py
@@ -1,14 +1,10 @@
-import hashlib
-
 from passlib.context import CryptContext
 
 
 pwd_context = CryptContext(schemes=['sha256_crypt'], sha256_crypt__rounds=10_000,deprecated='auto')
 
 def hash_password(password: str) -> str:
-    password_hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
-    return pwd_context.hash(password_hash)
+    return pwd_context.hash(password)
 
 def verify_password(password: str, hashed: str) -> bool:
-    password_hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
-    return pwd_context.verify(password_hash, hashed)
+    return pwd_context.verify(password, hashed)
