bdf8bf6bd4033ce43adbba788d7319440808c1da
Despierto <nrilock@gmail.com>
2026-08-10 12:59:28 +0300

    test: add GET /items/search tests (not found, single, multiple)

--- tests/integration/test_items_read.py
+++ tests/integration/test_items_read.py
@@ -28,3 +28,40 @@ async def test_item_get_success(client, session, item_id: int = 2):
     assert data['description'] == 'No description'
     assert data['price'] == 200
     assert data['stock_quantity'] == 5
+
+async def test_item_search_not_found(client, session):
+    response = await client.get('/items/search?item_search=Ноутбук')
+    assert response.status_code == 404
+
+async def test_item_search_success(client, session):
+    await client.post('/items/item', json={"name": "Item1", "price": 100, "stock_quantity": 10})
+    await client.post('/items/item', json={"name": "Ноутбук", "price": 500, "stock_quantity": 3})
+
+    response = await client.get('/items/search?item_search=Ноутбук')
+    assert response.status_code == 200
+    data = response.json()
+    assert len(data) == 1
+    assert data[0]['name'] == "Ноутбук"
+    assert data[0]['price'] == 500
+    assert data[0]['stock_quantity'] == 3
+    assert data[0]['description'] == 'No description'
+
+async def test_item_search_multiple(client, session):
+    await client.post('/items/item', json={"name": "Ноутбук", "price": 500, "stock_quantity": 3})
+    await client.post('/items/item', json={"name": "Item2", "price": 100, "stock_quantity": 10})
+    await client.post('/items/item', json={"name": "Ноутбук Pro", "price": 700, "stock_quantity": 2, "description": "test description"})
+
+    response = await client.get('/items/search?item_search=Ноутбук')
+    assert response.status_code == 200
+    data = response.json()
+    assert len(data) == 2
+
+    assert data[0]['name'] == "Ноутбук"
+    assert data[0]['price'] == 500
+    assert data[0]['stock_quantity'] == 3
+    assert data[0]['description'] == 'No description'
+
+    assert data[1]['name'] == "Ноутбук Pro"
+    assert data[1]['price'] == 700
+    assert data[1]['stock_quantity'] == 2
+    assert data[1]['description'] == 'test description'
