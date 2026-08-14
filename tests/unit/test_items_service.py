40016f727c90f2ff6700f756c0f4dd851639ce5f
Despierto <nrilock@gmail.com>
2026-08-14 20:59:37 +0300

    test: add unit tests for ItemService (get, patch, delete, search, get_all)

--- tests/unit/test_items_service.py
+++ tests/unit/test_items_service.py
@@ -5,7 +5,7 @@ from unittest.mock import patch, AsyncMock, ANY
 
 from app.models.items import Item
 from app.services.item_service import ItemService
-from app.schemas.item import ItemAdd
+from app.schemas.item import ItemAdd, ItemUpdate
 
 
 async def test_item_add_success():
@@ -129,3 +129,213 @@ async def test_item_add_all_full_duplicate():
     assert result['skipped'][0].name == 'Item1'
     assert result['skipped'][1].price == 200
     assert result['skipped'][1].stock_quantity == 20
+
+async def test_item_patch_not_found():
+    item_data = ItemUpdate(name='Test', stock_quantity=10)
+
+    mock_find = AsyncMock()
+
+    with patch('app.repository.item_repository.ItemRepository.find_by_id', mock_find):
+        mock_find.return_value = None
+        with pytest.raises(HTTPException) as exc_info:
+            await ItemService.patch_item(1, item_data, session=AsyncMock())
+
+    mock_find.assert_called_once_with(1, ANY)
+
+    assert exc_info.value.status_code == 404
+    assert 'не найден' in exc_info.value.detail
+
+async def test_item_patch_success():
+    item1 = Item(id=1, name='Item1', price=100, stock_quantity=10)
+    item_data = ItemUpdate(name='Test', stock_quantity=111)
+
+    mock_find = AsyncMock()
+    mock_raise = AsyncMock()
+
+    with patch('app.repository.item_repository.ItemRepository.find_by_id', mock_find):
+        with patch('app.utils.exceptions.ErrorHandler.raise_already_exists', mock_raise):
+            mock_find.return_value = item1
+
+            await ItemService.patch_item(1, item_data, session=AsyncMock())
+
+    mock_raise.assert_not_called()
+    mock_find.assert_called_once_with(1, ANY)
+
+    assert item1.id == 1
+    assert item1.name == 'Test'
+    assert item1.price == 100
+    assert item1.stock_quantity == 111
+
+async def test_item_patch_empty():
+    item1 = Item(id=1, name='Item1', price=100, stock_quantity=10)
+    item_data = ItemUpdate()
+
+    mock_find = AsyncMock()
+    mock_raise = AsyncMock()
+
+    with patch('app.repository.item_repository.ItemRepository.find_by_id', mock_find):
+        with patch('app.utils.exceptions.ErrorHandler.raise_already_exists', mock_raise):
+            mock_find.return_value = item1
+
+            await ItemService.patch_item(1, item_data, session=AsyncMock())
+
+    mock_raise.assert_not_called()
+    mock_find.assert_called_once_with(1, ANY)
+
+    assert item1.id == 1
+    assert item1.name == 'Item1'
+    assert item1.price == 100
+    assert item1.stock_quantity == 10
+
+async def test_item_delete_success():
+    item1 = Item(id=1, name='Item1', price=100, stock_quantity=10)
+
+    mock_find = AsyncMock()
+    mock_raise = AsyncMock()
+
+    with patch('app.repository.item_repository.ItemRepository.find_by_id', mock_find):
+        with patch('app.utils.exceptions.ErrorHandler.raise_not_found', mock_raise):
+            mock_find.return_value = item1
+
+            result = await ItemService.del_item(1, session=AsyncMock())
+
+    mock_raise.assert_not_called()
+    mock_find.assert_called_once_with(1, ANY)
+
+    assert 'удален из базы' in result
+
+async def test_item_delete_not_found():
+    mock_find = AsyncMock()
+
+    with patch('app.repository.item_repository.ItemRepository.find_by_id', mock_find):
+        mock_find.return_value = None
+        with pytest.raises(HTTPException) as exc_info:
+            await ItemService.del_item(1, session=AsyncMock())
+
+    mock_find.assert_called_once_with(1, ANY)
+
+    assert exc_info.value.status_code == 404
+    assert 'не найден' in exc_info.value.detail
+
+async def test_item_get_success():
+    item1 = Item(id=1, name='Item1', price=100, stock_quantity=10)
+
+    mock_find = AsyncMock()
+    mock_raise = AsyncMock()
+
+    with patch('app.repository.item_repository.ItemRepository.find_by_id', mock_find):
+        with patch('app.utils.exceptions.ErrorHandler.raise_not_found', mock_raise):
+            mock_find.return_value = item1
+
+            result = await ItemService.get_item(1, session=AsyncMock())
+
+    mock_raise.assert_not_called()
+    mock_find.assert_called_once_with(1, ANY)
+
+    assert result.id == 1
+    assert result.name == 'Item1'
+    assert result.price == 100
+    assert result.stock_quantity == 10
+
+async def test_item_get_not_found():
+    mock_find = AsyncMock()
+
+    with patch('app.repository.item_repository.ItemRepository.find_by_id', mock_find):
+        mock_find.return_value = None
+
+        with pytest.raises(HTTPException) as exc_info:
+            await ItemService.get_item(1, session=AsyncMock())
+
+    mock_find.assert_called_once_with(1, ANY)
+
+    assert exc_info.value.status_code == 404
+    assert 'не найден' in exc_info.value.detail
+
+async def test_item_get_all_success():
+    item1 = Item(id=1, name='Item1', price=100, stock_quantity=10)
+    item2 = Item(id=2, name='Item2', price=200, stock_quantity=20)
+    item3 = Item(id=3, name='Item3', price=300, stock_quantity=30)
+
+    mock_find = AsyncMock()
+
+    with patch('app.repository.item_repository.ItemRepository.find_all', mock_find):
+        mock_find.return_value = [item1, item2, item3]
+
+        result = await ItemService.get_all(session=AsyncMock())
+
+    mock_find.assert_called_once_with(ANY)
+
+    assert len(result) == 3
+    assert result[0].id == 1
+    assert result[1].name == 'Item2'
+    assert result[2].price == 300
+    assert result[2].stock_quantity == 30
+
+async def test_item_get_all_empty():
+    mock_find = AsyncMock()
+
+    with patch('app.repository.item_repository.ItemRepository.find_all', mock_find):
+        mock_find.return_value = []
+
+        result = await ItemService.get_all(session=AsyncMock())
+
+    mock_find.assert_called_once_with(ANY)
+
+    assert len(result) == 0
+
+async def test_item_search_success():
+    item1 = Item(id=1, name='Test', price=100, stock_quantity=10)
+
+    mock_find = AsyncMock()
+    mock_raise = AsyncMock()
+
+    with patch('app.repository.item_repository.ItemRepository.search_items', mock_find):
+        with patch('app.utils.exceptions.ErrorHandler.raise_not_found', mock_raise):
+            mock_find.return_value = [item1]
+
+            result = await ItemService.search_items('Test', session=AsyncMock())
+
+    mock_find.assert_called_once_with('Test', ANY)
+    mock_raise.assert_not_called()
+
+    assert result[0].id == 1
+    assert result[0].name == 'Test'
+    assert result[0].price == 100
+    assert result[0].stock_quantity == 10
+
+async def test_item_search_multiple_success():
+    item1 = Item(id=1, name='Test', price=100, stock_quantity=10)
+    item2 = Item(id=2, name='Test second', price=200, stock_quantity=20)
+    item3 = Item(id=3, name='Test3', price=300, stock_quantity=30)
+
+    mock_find = AsyncMock()
+    mock_raise = AsyncMock()
+
+    with patch('app.repository.item_repository.ItemRepository.search_items', mock_find):
+        with patch('app.utils.exceptions.ErrorHandler.raise_not_found', mock_raise):
+            mock_find.return_value = [item1, item2, item3]
+
+            result = await ItemService.search_items('Test', session=AsyncMock())
+
+    mock_find.assert_called_once_with('Test', ANY)
+    mock_raise.assert_not_called()
+
+    assert len(result) == 3
+    assert result[0].id == 1
+    assert result[1].name == 'Test second'
+    assert result[2].price == 300
+    assert result[0].stock_quantity == 10
+
+async def test_item_search_not_found():
+    mock_find = AsyncMock()
+
+    with patch('app.repository.item_repository.ItemRepository.search_items', mock_find):
+        mock_find.return_value = []
+
+        with pytest.raises(HTTPException) as exc_info:
+            await ItemService.search_items('Test', session=AsyncMock())
+
+    mock_find.assert_called_once_with('Test', ANY)
+
+    assert exc_info.value.status_code == 404
+    assert 'не найден' in exc_info.value.detail
